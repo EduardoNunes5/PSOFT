790dfdd4454ba2db81c7dbfc4c543b8a74921c11
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2020-05-03 17:07:35 -0300

    ok

--- PSOFT-GuessTheCapital/script.js
+++ PSOFT-GuessTheCapital/script.js
@@ -5,7 +5,7 @@ let states,
 
 
 function startGame(){
-	fetch("raw.githubusercontent.com/EduardoNunes5/PSOFT/master/PSOFT-GuessTheCapital/estados.json")
+	fetch("https://raw.githubusercontent.com/EduardoNunes5/PSOFT/master/PSOFT-GuessTheCapital/estados.json")
 	.then(resolve => resolve.json())
 	.then(statesJson => {
 		states = statesJson;
