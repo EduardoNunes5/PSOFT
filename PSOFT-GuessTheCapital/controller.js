5c323f2c4608e99b8a93eb48228e5ea64f54b452
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2020-05-08 23:09:34 -0300

    quebrei

--- PSOFT-GuessTheCapital/controller.js
+++
@@ -1,9 +0,0 @@
-import { getStates } from './model.js';
-import { render } from './template.js'; 
-
-
-$state = document.querySelector("#state");
-
-estados = getStates();
-
-render($state, estados[1].estados[0].estados)
